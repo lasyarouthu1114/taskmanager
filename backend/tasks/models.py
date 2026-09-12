51568b8bc3071f317bcf9ca839ff3ae92bf842df
Lasya Routhu <lasyarouthu1114@gmail.com>
2026-09-12 17:49:13 +0530

    Initial Django backend setup with Task model

---
+++ backend/tasks/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    due_date = models.DateField(null=True, blank=True)
+    priority = models.IntegerField(default=0)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
